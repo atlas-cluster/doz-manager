5f0d06d165d9e2e3ebe71b7cfcbf221e80054978
Luca Wahlen <mail@lucawahlen.de>
2026-01-22 09:49:01 +0100

    Revert "fix tests"
    This reverts commit 3d25a4406282ec244db37b13b07f17c79f0b3ff1.

--- app/(app)/lecturers/page.test.tsx
+++ app/(app)/lecturers/page.test.tsx
@@ -1,8 +1,8 @@
-import { test, vi } from 'vitest'
+import { expect, test, vi } from 'vitest'
 
 import LecturersPage from '@/app/(app)/lecturers/page'
 import { prisma } from '@/features/shared/lib/prisma'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 // Mock Prisma
 vi.mock('@/features/shared/lib/prisma', () => ({
@@ -13,9 +13,19 @@ vi.mock('@/features/shared/lib/prisma', () => ({
   },
 }))
 
-test('LecturersPage renders', async () => {
+// Mock Feature Components to skip rendering children
+vi.mock('@/features/lecturers', () => ({
+  CreateLecturerDialog: () => <div data-testid="create-dialog" />,
+  LecturerDataTable: () => <div data-testid="data-table" />,
+  lecturerColumns: [],
+}))
+
+test('LecturersPage renders main components', async () => {
   vi.mocked(prisma.lecturer.findMany).mockResolvedValue([])
 
   const Page = await LecturersPage()
   render(Page)
+
+  expect(screen.getByTestId('create-dialog')).toBeDefined()
+  expect(screen.getByTestId('data-table')).toBeDefined()
 })
