974ec43317cdce0ccc7067137dd80ffa49be9f2a
Luca Wahlen <mail@lucawahlen.de>
2026-01-23 11:23:15 +0100

    chore: migrate from Bun to npm for package management and update CI workflows

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-    seed: 'bun prisma/seed.ts',
+    seed: 'npm prisma/seed.ts',
   },
   datasource: {
     url: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?schema=public`,
