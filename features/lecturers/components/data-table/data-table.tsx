7325cc4ee7d294b76eb06f18c23fe9c833a8fd1f
Luca Wahlen <46722254+LucaWahlen@users.noreply.github.com>
2026-02-09 10:17:31 +0000

    fix: adjust alignment of items in DataTable component for better layout

--- features/lecturers/components/data-table/data-table.tsx
+++ features/lecturers/components/data-table/data-table.tsx
@@ -252,7 +252,7 @@ export function DataTable({
 
   return (
     <div className="w-full space-y-3">
-      <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+      <div className="flex flex-col md:flex-row md:items-start md:justify-between">
         <div className="flex w-full flex-wrap items-center gap-2">
           <div className="flex w-full gap-2 md:w-64">
             {/** Desktop only: Show input only */}
